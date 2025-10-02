0a0a42827a12b0dfdbe0a5dddd46857ea94f3e3d
John Comeau <jc@unternet.net>
2025-10-01 20:19:49 -0700

    working when stracing, otherwise not seeing byte count from stdin

--- raw.c
+++ raw.c
@@ -21,7 +21,6 @@ int main(int argc, char **argv) {
 	int bytes, count, available;
 	unsigned char buffer[BUFFERSIZE];
 	unsigned char *pointer = buffer;
-	fprintf(stderr, "args: %d ", argc);
 	if (argc == 1) {  /* get data from stdin */
 		if (ioctl(STDIN_FILENO, FIONREAD, &bytes) != 0) {
 			perror("failed getting byte count");
@@ -29,14 +28,15 @@ int main(int argc, char **argv) {
 		} else if (bytes == 0) {
 			fprintf(stderr, "no bytes found in stdin\n");
 			return 1;
+		} else {
+			fprintf(stderr, "reading stdin into buffer\n");
 		}
-		fprintf(stderr, "reading stdin into buffer\n");
-		if (count = fread(
+		if ((count = fread(
 			buffer,
 			1,
 			MIN(BUFFERSIZE, bytes),
 			stdin
-		) != bytes)
+		)) != bytes)
 			fprintf(stderr, "read %d out of %d bytes\n",
 				count, bytes);
 		else fprintf(stderr, "got %d bytes from stdin\n", count);
